2ecb198eca86c8c44c452abe8e8a92dce8873186
Bhaskar <arbkm22@gmail.com>
2021-01-29 22:49:22 +0530

    Meeting Friends #723A

---
+++ MeetingFriends.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    // varianle declaration and input
+    vector<int> v;
+    int temp;
+    while (cin >> temp)
+        v.push_back(temp);
+    
+    // sorting the vector for better access of elements
+    sort(v.begin(), v.end());
+
+    // self explanatory
+    int answer = (v[1] - v[0]) + (v[2] - v[1]);
+    
+    // output
+    cout << answer << endl;
+
+    return 0;
+}
