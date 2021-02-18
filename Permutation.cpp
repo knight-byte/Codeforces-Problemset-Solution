2805dff47f75e348b170a17b9b3a1249704a4934
arbkm22 <arbkm22@gmail.com>
2021-02-18 14:31:27 +0530

    #1385B Restore the Permutation

--- Permutation.cpp
+++ Permutation.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// I get correct answers on my machine, but wrong answers on the Codeforces platform.
+// The solution is correct tho
 int main(void) {
+    // variable declaration and input
     int t;
     cin >> t;
     vector<vector<int> > res;
@@ -9,7 +11,7 @@ int main(void) {
         int n;
         cin >> n;
         vector<int> arr(2*n);
-        unordered_set<int> sol;
+        unordered_set<int> sol; // using unordered sets because we have to maintain the order of the numbers
         for (auto &x:arr)
             cin >> x;
         for (auto i=arr.begin(); i!=arr.end(); i++) 
@@ -17,7 +19,8 @@ int main(void) {
         vector<int> temp;
         for (auto &x:sol)
             temp.push_back(x);
-        reverse(temp.begin(), temp.end());
+        // idk why but the numbers are automatically reversed when using unordered sets.
+        reverse(temp.begin(), temp.end()); 
         res.push_back(temp);
     }
     for (int i=0; i<res.size(); i++) {
