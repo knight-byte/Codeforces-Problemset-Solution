7e8111706dcf2f3845e0aba2885999e23d521cda
arbkm22 <arbkm22@gmail.com>
2021-02-23 14:56:15 +0530

    #1186A - Vus the Cossack and a Contest

---
+++ VusCossack.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int n, m, k;
+    cin >> n >> m >> k;
+    if (m<n || k<n)
+        cout << "NO";
+    else 
+        cout << "YES";
+    cout << endl;
+
+    return 0;
+}
