05c31cc49affc813139773077ddadb7b77fd844b
arbkm22 <arbkm22@gmail.com>
2021-02-25 08:46:45 +0530

    #136A - Presents

--- Presents.cpp
+++ Presents.cpp
@@ -5,9 +5,14 @@ int main(void) {
     int n;
     cin >> n;
     int arr[n];
-    int res[n];
-    for (int i=0; i<n; i++) 
+    int sol[n];
+    for (int i=0; i<n; i++)
         cin >> arr[i];
+    for (int i=0; i<n; i++)
+        sol[arr[i]-1] = i+1;
+    for (int i=0; i<n; i++)
+        cout << sol[i] << " ";
+    cout << endl;
     
     return 0;
 }
