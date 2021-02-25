91ce3d38f9daf6935503c04b214ea3f77ac086b0
arbkm22 <arbkm22@gmail.com>
2021-02-25 10:19:06 +0530

    #148A - Insomnia Cure

--- InsomniaCure.cpp
+++ InsomniaCure.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main(void) {
-    int k, l, m, n, d;
+    int k, l, m, n;
+    long long d;
+    set<long long> damaged;
     cin >> k >> l >> m >> n >> d;
-    // To be solved.
+    if (d<k || d<l || d<m || d<n)
+        cout << 0 << endl;
+    else if (k==1)
+        cout << d << endl;
+    else {
+        for (long long i=1; i<=d; i++) {
+            if (i%k==0) {
+                damaged.insert(i);
+                continue;
+            }
+            else if (i%l==0) {
+                damaged.insert(i);
+                continue;
+            }
+            else if (i%m==0) {
+                damaged.insert(i);
+                continue;
+            }
+            else if (i%n==0) {
+                damaged.insert(i);
+                continue;
+            }
+        }
+        cout << damaged.size() << endl;
+    }
     return 0;
 }
