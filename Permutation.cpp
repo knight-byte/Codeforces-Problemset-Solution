33389e0d2412215d0052b58a7f1f89545f63dd9e
arbkm22 <arbkm22@gmail.com>
2021-02-27 12:51:26 +0530

    #1385B - Restore the Permutation by Merger

--- Permutation.cpp
+++ Permutation.cpp
@@ -31,3 +31,47 @@ int main(void) {
 
     return 0;
 }
+// Bruh moment, tried another method, but it keeps on failing
+// will improve later.
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(vector<int> arr, int l, int r, int x) {
+    if (r>1) {
+        int mid = l+(r-l)/2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binary_search(arr, l, mid-1, x);
+        return binary_search(arr, mid+1, r, x);
+    }
+    return -1;
+}
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(2*n);
+        for (auto &x:arr)
+            cin >> x;
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for (int i=1; i<arr.size(); i++) {
+            int state = binary_search(ans, 0, ans.size(), arr[i]);
+            if (state == -1)
+                ans.push_back(arr[i]);
+            else 
+                continue;
+        }
+        for (auto &x:ans)
+            cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
