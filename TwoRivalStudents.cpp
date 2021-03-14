85e4567e365cfef8f8b48ec901ea68e82dd8c21f
arbkm22 <arbkm22@gmail.com>
2021-03-14 13:09:08 +0530

    #1108A - Two Rival Students

---
+++ TwoRivalStudents.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x, a, b;
+        cin >> n >> x >> a >> b;
+        int ans = min(n-1, abs(a-b) + x);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
