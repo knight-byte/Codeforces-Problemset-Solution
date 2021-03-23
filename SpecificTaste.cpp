3610f50899f5ec8f4640cd4fc714f3d3c643c9b6
arbkm22 <arbkm22@gmail.com>
2021-03-24 01:55:44 +0530

    #1438A - Specific Tastes of Andre

---
+++ SpecificTaste.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i=0; i<n; i++)
+            a.push_back(1); // what the fuck
+        for (auto &x:a)
+            cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
