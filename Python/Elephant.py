03c04f2999a3c0933c36f73e601b10b7b4c61392
knight-btye <abunachar1236@gmail.com>
2021-04-04 17:16:48 +0530

    Elephant

--- Python/Elephant.py
+++ Python/Elephant.py
@@ -1,16 +1,13 @@
-def steps_counter(coord):
-    steps = 0
-    while coord > 0:
-        coord -= 5
-        steps += 1
-    return steps
-
-
 def main():
-    coordinates = int(input())
-    output = steps_counter(coordinates)
-    print(output)
+    total_step = int(input())
+    count_step = 0
+    for i in range(5, 1, -1):
+        count_step += total_step//i
+        total_step = total_step % i
+    count_step += total_step
+
+    print(count_step)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
