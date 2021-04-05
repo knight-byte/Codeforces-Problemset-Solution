0d849d9047f16ee5440a5182c31c28f4ea54dc91
arbkm22 <arbkm22@gmail.com>
2021-04-05 22:22:10 +0530

    #1093A - Dice Rolling

---
+++ DiceRolling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        cout << x/2 << endl;
+    }
+
+    return 0;
+}
