2a4c6e030386303b99694a1d90421547b83cbabf
knight-btye <abunachar1236@gmail.com>
2021-04-06 22:00:54 +0530

    TwoArraysSwaps

--- Python/TwoArraysSwaps.py
+++ Python/TwoArraysSwaps.py
@@ -1,22 +1,14 @@
 def main():
     t = int(input())
-    sol = []
-    while t:
-        t -= 1
-        n = int(input())
-        k = int(input())
-        a = sorted([int(input()) for i in range(n)])
-        b = sorted([int(input()) for i in range(n)])
-        ans = 0
-        for i in range(n):
-            if i < k:
-                ans += max(a[i], b[i])
-            else:
-                ans += a[i]
-
-        sol.append(ans)
-    for i in sol:
-        print(i)
+    for _ in range(t):
+        n, k = map(int, input().split())
+        a = sorted(list(map(int, input().split())))
+        b = sorted(list(map(int, input().split())), reverse=True)
+        for i in range(k):
+            if a[i] >= b[i]:
+                break
+            a[i] = b[i]
+        print(sum(a))
 
 
 if __name__ == '__main__':
