67f48283e460e1401f32a54d1633c350c45ec80a
knight-btye <abunachar1236@gmail.com>
2021-04-19 11:44:23 +0530

    String_Similarity

---
+++ Python/A_String_Similarity.py
@@ -0,0 +1,17 @@
+'''
+Author     : knight_byte
+File       : A_String_Similarity.py
+Created on : 2021-04-18 13:05:02
+'''
+
+
+def main():
+    n = int(input())
+    s = input()
+    print(s[::2])
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        main()
