8bb4f6827d238b647de10b2e84a3c3cf5ddb97db
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-19 19:21:27 +0530

    #1015A - Points in Segments

---
+++ PointsInSegments.cpp
@@ -0,0 +1,56 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author		: hellking
+File		: PointsInSegments
+Created on	: Mon, 19 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m+1);
+    for (int i=1; i<=n; i++) {
+        int l, r;
+        cin >> l >> r;
+        for (int i=l; i<=r; i++)
+            a[i]++;
+    }
+    vector<int> ans;
+    for (int i=1; i<=m; i++) {
+        if (a[i]==0)
+            ans.push_back(i);
+    }
+    cout << ans.size() << endl;
+    for (auto &x:ans)
+        cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
