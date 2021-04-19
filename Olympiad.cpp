f95fcccfb39033a780d7b39834e65c50cfb5b804
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-19 18:03:48 +0530

    #937A - Olympiad

---
+++ Olympiad.cpp
@@ -0,0 +1,49 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author		: hellking
+File		: Olympiad
+Created on	: Mon, 19 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x:a)
+        cin >> x;
+    set<int> ans;
+    for (int i=0; i<n; i++) {
+        if (a[i]!=0)
+            ans.insert(a[i]);
+    }
+    cout << ans.size() << endl;
+
+    return 0;
+}
