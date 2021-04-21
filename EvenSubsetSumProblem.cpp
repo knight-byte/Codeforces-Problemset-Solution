7d6460e259f420ec0828b2d1ef8371bc7f9331da
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-21 09:51:54 +0530

    #1323A - Even Subset Even Sum Problem

---
+++ EvenSubsetSumProblem.cpp
@@ -0,0 +1,64 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author		: hellking
+File		: EvenSubsetSumProblem
+Created on	: Wed, 21 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto &x:a)
+            cin >> x;
+        vector<int> odd, even;
+        for (int i=0; i<n; i++) {
+            if (a[i]%2==0)
+                even.push_back(i+1);
+            else 
+                odd.push_back(i+1);
+        }
+        if (even.size()>=1) {
+            cout << 1 << endl;
+            cout << even[0] << endl;
+        }
+        else if (odd.size()>=2) {
+            cout << 2 << endl;
+            cout << odd[0] << " " << odd[1] << endl;
+        }
+        else 
+            cout << -1 << endl;
+    }
+
+    return 0;
+}
