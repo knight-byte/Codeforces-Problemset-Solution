fd3d5e5b5df95b04a8dbafaed40e5ec19da3aa2e
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-22 00:31:36 +0530

    #34A - Reconnaissance 2

---
+++ Reconnaissance2.cpp
@@ -0,0 +1,58 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : Reconnaissance2
+Created on  : Wed, 21 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x:a)
+        cin >> x;
+    int diff = 100000;
+    pair<int, int> ans;
+    for (int i=0; i<n-1; i++) {
+        int temp = abs(a[i]-a[i+1]);
+        if (temp<diff) {
+            diff = temp;
+            ans.first = i+1;
+            ans.second = i+2;
+        }
+    }
+    int temp = abs(a[0]-a[n-1]);
+    if (temp<diff) {
+        ans.first = 1;
+        ans.second = n;
+    }
+    cout << ans.first << " " << ans.second << endl;
+    return 0;
+}
