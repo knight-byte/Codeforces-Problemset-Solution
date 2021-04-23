d41fc4284a84b9fae7af4e2174a80105dba363c4
knight-btye <abunachar1236@gmail.com>
2021-04-23 10:44:34 +0530

    Strange_Functions

---
+++ Python/A_Strange_Functions.py
@@ -0,0 +1,16 @@
+'''
+Author     : knight_byte
+File       : A_Strange_Functions.py
+Created on : 2021-04-23 09:55:54
+'''
+
+
+def main():
+    n = input()
+    print(len(n))
+
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        main()
