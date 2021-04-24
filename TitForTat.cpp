d9c6e030812bc6cd137a03fff841933e02a7a38a
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-24 21:38:11 +0530

    #1516A - Tit for Tat

---
+++ TitForTat.cpp
@@ -0,0 +1,62 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : TitForTat
+Created on  : Sat, 24 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (auto &x:a)
+            cin >> x;
+        for (int i=0; i<n-1; i++) {
+            if (a[i]<k) {
+                k -= a[i];
+                a[n-1] += a[i];
+                a[i] = 0;
+            }
+            else {
+                a[i] -= k;
+                a[n-1] += k;
+                k = 0;
+            }
+        }
+        for (auto &x:a)
+            cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
