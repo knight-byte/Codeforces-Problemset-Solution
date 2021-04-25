d2d2d12049a51d10eb867deb87e54bf06f96742a
Bhaskar Mahto <arbkm22@gmail.com>
2021-04-25 23:24:54 +0530

    #994A - Fingerprints

---
+++ Fingerprints.cpp
@@ -0,0 +1,52 @@
+/*
+      .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+ 
+Author      : hellking
+File        : Fingerprints
+Created on  : Sun, 25 April, 2021
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (auto &x:a)
+        cin >> x;
+    for (auto &y:b)
+        cin >> y;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (a[i] == b[j]) 
+                cout << a[i] << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
